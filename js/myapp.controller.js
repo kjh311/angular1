1bb66eeeecf3ffd119c5343eb752057941f09f73
Kevin Huelsmann <kjh311@gmail.com>
2016-09-04 12:31:39 -0700

    index

--- js/myapp.controller.js
+++ js/myapp.controller.js
@@ -1,8 +1,6 @@
 angular.module("myapp", [])
 
   .controller("Controller1", function($scope){
-    $scope.fuckityfuck = {};
-    $scope.fuckityfuck.blah = "FuckityFuckFuckFuck";
     $scope.myText="Kevin is the man!";
   })
 
