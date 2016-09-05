67ec73d7b6dbfcd2c967edcb4730c32bfd4b09a2
Kevin Huelsmann <kjh311@gmail.com>
2016-09-05 06:47:05 -0700

    ng-repeat

--- js/myapp.controller.js
+++ js/myapp.controller.js
@@ -2,6 +2,7 @@ angular.module("myapp", [])
 
   .controller("Controller1", function($scope){
     $scope.myText="Kevin is the man!";
+    $scope.countries = "[{locale:'en-US',name:'United States'}, {locale:'en-GB',name:'United Kingdom'}, {locale:'en-FR',name:'France'}]";
   })
 
 
