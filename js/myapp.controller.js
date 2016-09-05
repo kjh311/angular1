17c7cde0112ef1d7edee2366df77d313eb9614d1
Kevin Huelsmann <kjh311@gmail.com>
2016-09-05 07:09:04 -0700

    functions in controller

--- js/myapp.controller.js
+++ js/myapp.controller.js
@@ -10,4 +10,21 @@ angular.module("myapp", [])
   .controller("HelloController", function($scope){
     $scope.helloTo = {};
     $scope.helloTo.title = "AngularJS";
+    $scope.student = {
+      firstName: "Kevin",
+      lastName: "Bad Mother Fucker",
+      fees: 454,
+      books: 299,
+      subjects:[
+                  {name:'Physics',marks:70},
+                  {name:'Chemistry',marks:80},
+                  {name:'Math',marks:65}
+               ],
+
+      fullName: function() {
+        var studentObject;
+        studentObject = $scope.student;
+        return studentObject.firstName + " " + studentObject.lastName;
+      }
+    };
   });
